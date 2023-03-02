0f2a63baef82b550c3853da2e3ed6f562e91063b
Nino Lefort <nino.lefort@mmibordeaux.com>
2023-03-02 09:19:09 +0100

    Bug image aucun charactere ecrit done

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -65,6 +65,6 @@ class PlaylistsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def playlist_params
-      params.require(:playlist).permit(:song, :name, :name)
+      params.require(:playlist).permit(:song, :name, :image)
     end
 end
